298a26ae0181e2132588e077ef0447fff71bcf10
jurkotroll <jurko.bossetroll@gmail.com>
2017-09-05 23:10:55 +0200

    first commit: start

---
+++ project.clj
@@ -0,0 +1,51 @@
+(defproject sec-test "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.229"]
+                 [reagent "0.7.0"]
+                 [reagent-utils "0.2.1"]
+                 [hiccup "1.0.5"]
+                 [secretary "1.2.3"]
+                 [venantius/accountant "0.2.0"]
+                 [bidi "2.1.1"]]
+
+  :min-lein-version "2.5.3"
+
+  :source-paths ["src/clj"]
+
+  :plugins [[lein-cljsbuild "1.1.4"]]
+
+  :clean-targets ^{:protect false} ["resources/public/js"
+                                    "target"]
+
+  :figwheel {:css-dirs ["resources/public/css"]}
+
+  :profiles
+  {:dev
+   {:dependencies []
+
+    :plugins      [[lein-figwheel "0.5.10"]]
+    }}
+
+  :cljsbuild
+  {:builds
+   [{:id           "dev"
+     :source-paths ["src/cljs"]
+     :figwheel     {:on-jsload "sec-test.core/reload"}
+     :compiler     {:main                 sec-test.core
+                    :optimizations        :none
+                    :output-to            "resources/public/js/app.js"
+                    :output-dir           "resources/public/js/dev"
+                    :asset-path           "js/dev"
+                    :source-map-timestamp true}}
+
+    {:id           "min"
+     :source-paths ["src/cljs"]
+     :compiler     {:main            sec-test.core
+                    :optimizations   :advanced
+                    :output-to       "resources/public/js/app.js"
+                    :output-dir      "resources/public/js/min"
+                    :elide-asserts   true
+                    :closure-defines {goog.DEBUG false}
+                    :pretty-print    false}}
+
+    ]})
---
+++ src/clj/sec_test/core.clj
@@ -0,0 +1,1 @@
+(ns sec-test.core)
